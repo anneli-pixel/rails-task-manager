68ca7e03dc12d21b376c3cce1c3c07e0e59d32c2
Anne Lißner <anne_lissner@gmx.net>
2020-06-20 15:09:27 +0200

    added route to create a task

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # As a user, I can list tasks
   get 'tasks', to: 'tasks#index'
+  # As a user, I can add a new task
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
   # As a user, I can view the details of a task
   get 'tasks/:id', to: 'tasks#show', as: :task
-  # As a user, I can add a new task
   # As a user, I can edit a task (mark as completed / update title & details)
   # As a user, I can remove a task
 end
